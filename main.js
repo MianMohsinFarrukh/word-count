920b66adac1f7ff2e07a2cbd6a08a60a6d075887
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-17 10:45:20 -0700

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
-
 import inquirer from "inquirer";
-
-
-const answer: { sentence: string } = await inquirer.prompt([
+const answer = await inquirer.prompt([
     {
         name: "sentence",
         type: "input",
         message: "Enter your Sentence to count the word :",
     }
-
 ]);
-
 const words = answer.sentence.trim().split(" ");
-
 console.log(words);
-
 console.log(`Your Sentence word count is : ${words.length}`);
-
-
